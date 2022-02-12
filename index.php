45b3f8680b8802ddfd2d4d32c2faed483222e175
Victor <velikii20020416@gmail.com>
2022-02-12 12:51:05 +0200

    Обновленный php файл, с абстрактным классом и методом

--- index.php
+++ index.php
@@ -80,42 +80,74 @@
 // $obj -> func();
 
 
-class Car {
+abstract class Car {
     // private ?int $position = null;
 
     public function __construct(
-        private string $brand,
-        private string $model,
+        protected string $brand,
+        protected string $model,
         private ?int $position = 5
     )
-    {}
+    {
+        // echo __CLASS__.':'.__METHOD__."<br>";
+    }
 
     public function getPosition() : ?int {
+        // echo __CLASS__.':'.__METHOD__."<br>";
         return $this->position;
     }
 
     public function move(int $speed, int $time) : void{
+        // echo __CLASS__.':'.__METHOD__."<br>";
         $this->position += $speed * $time;
+    }
+
+    abstract public function getModel() : string;
+}
+
+
+class Lexus extends Car {
+    public function __construct(string $model)
+    {
+        // echo __CLASS__.':'.__METHOD__."<br>";
+        parent::__construct('Lexus', $model);
+    }
+    public function getModel() : string {
+        // echo __CLASS__.':'.__METHOD__."<br>";
+        return $this->model . ' ' . $this->brand;
+    }
+}
+
+class Porsche extends Car {
+    public function __construct(string $model)
+    {
+        // echo __CLASS__.':'.__METHOD__."<br>";
+        parent::__construct('Porsche', $model);
+    }
+
+    public function getModel() : string {
+        // echo __CLASS__.':'.__METHOD__."<br>";
+        return $this->brand . ' ' . $this->model;
     }
 }
 
 
 require 'index.htm';
+// 
+$p911 = new Porsche('911');
+// echo $p911 -> getModel().'<br>';
 
-$p911 = new Car('Porshe', '911');
-echo $p911 -> getPosition().'<br>';
-
-$lexus = new Car('Lexus', '470');
-echo $lexus -> getPosition().'<br>';
+$lexus = new Lexus('470');
+// echo $lexus -> getModel().'<br>';
 
 if (!empty($_GET['speed']) && !empty($_GET['time'])) {
     $p911 -> move($_GET['speed'], $_GET['time']);
-    echo $p911 -> getPosition().'<br>';
+    echo $p911 -> getModel().'<br>';
 }
 
 if (!empty($_GET['speed']) && !empty($_GET['time'])) {
     $lexus -> move($_GET['speed'], $_GET['time']);
-    echo $lexus -> getPosition().'<br>';
+    echo $lexus -> getModel().'<br>';
 }
 
 
@@ -123,9 +155,6 @@ if (!empty($_GET['speed']) && !empty($_GET['time'])) {
 // echo $p911 -> getPosition().'<br>';
 // $p911 -> move(10, 50);
 // echo $p911 -> getPosition();
-
-
-
 
 
 
